ef54b91f4f321505ea25b49bcbaffee18dd1fc05
hieuduy1751 <hieuduy1751@gmail.com>
2022-10-13 00:08:22 +0700

    add: useReducer and useContext to make store state management

--- App.tsx
+++ App.tsx
@@ -1,66 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { Keyboard, FlatList, KeyboardAvoidingView, SafeAreaView, Text, TouchableWithoutFeedback, View, Platform } from 'react-native';
+import {
+  Keyboard,
+  FlatList,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  Platform,
+} from 'react-native';
 import Task from './components/Task';
 import TodoInput from './components/TodoInput';
-import StoreReducer, { actions, ApiService, Job } from './store'
-import { initialState } from './store/StoreReducer';
-import { useEffect, useReducer } from 'react';
+import StoreReducer, { actions, ApiService, initialState, Job } from './store';
+import { createContext, useEffect, useReducer } from 'react';
 
 export default function App() {
-  const [state, dispatch] = useReducer(StoreReducer, initialState)
-  const { jobs } = state
+  const [state, dispatch] = useReducer(StoreReducer, initialState);
+  const Context = createContext([state, dispatch]);
+  const { jobs } = state;
 
   const handleDissmissKeyboard = () => {
     Keyboard.dismiss();
-  }
+  };
 
   useEffect(() => {
     ApiService.getTasks().then((jobs: Job[]) => {
-      dispatch(actions.setJobs(jobs))
-    })
-  }, [])
+      dispatch(actions.setJobs(jobs));
+    });
+  }, []);
 
   return (
-    <SafeAreaView style={{
-      flex: 1,
-    }}>
-      <TouchableWithoutFeedback onPress={handleDissmissKeyboard}>
-        <KeyboardAvoidingView style={{
-          width: '100%',
-          paddingHorizontal: 50,
-          height: '100%',
+    <Context.Provider value={[state, dispatch]}>
+      <SafeAreaView
+        style={{
           flex: 1,
         }}
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
-          keyboardVerticalOffset={15}
-        >
-          <>
-            <View style={{
+      >
+        <TouchableWithoutFeedback onPress={handleDissmissKeyboard}>
+          <KeyboardAvoidingView
+            style={{
               width: '100%',
-              height: 100,
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}>
-              <Text style={{
-                fontSize: 30,
-                fontWeight: 'bold',
-              }}>TODO-App</Text>
-            </View>
-            <View style={{
+              paddingHorizontal: 50,
+              height: '100%',
               flex: 1,
-            }}>
-              <FlatList
-                data={jobs}
-                renderItem={({ item }: { item: Job }) => <Task job={item} dispatch={dispatch} />}
-                keyExtractor={(item: Job, index: number) => index.toString()}
-                showsVerticalScrollIndicator={false}
-              />
-            </View>
-            <TodoInput state={state} dispatch={dispatch} />
-            <StatusBar style="auto" />
-          </>
-        </KeyboardAvoidingView>
-      </TouchableWithoutFeedback>
-    </SafeAreaView>
+            }}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={15}
+          >
+            <>
+              <View
+                style={{
+                  width: '100%',
+                  height: 100,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 30,
+                    fontWeight: 'bold',
+                  }}
+                >
+                  TODO-App
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                }}
+              >
+                <FlatList
+                  data={jobs}
+                  renderItem={({ item }: { item: Job }) => (
+                    <Task job={item} dispatch={dispatch} />
+                  )}
+                  keyExtractor={(item: Job, index: number) => index.toString()}
+                  showsVerticalScrollIndicator={false}
+                />
+              </View>
+              <TodoInput state={state} dispatch={dispatch} />
+              <StatusBar style="auto" />
+            </>
+          </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+      </SafeAreaView>
+    </Context.Provider>
   );
 }
